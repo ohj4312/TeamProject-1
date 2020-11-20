a6f1ce3721e2e4f95d853262ca4468659e824a34
COM <COM@DESKTOP-TH36TT5>
2020-11-20 18:29:37 +0900

    db변경

--- src/main/java/com/mycompany/webapp/dao/PhotoDAO.java
+++ src/main/java/com/mycompany/webapp/dao/PhotoDAO.java
@@ -8,8 +8,15 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.webapp.dto.Register_photo;
+
 @Repository
 public class PhotoDAO {
 	@Resource
 	private SqlSessionTemplate sst;
+
+	public Register_photo insertRphoto(Register_photo rphoto) {
+		//sst.insert
+		return null;
+	}
 }
