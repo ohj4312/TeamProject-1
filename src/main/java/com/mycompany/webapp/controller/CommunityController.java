e000fdabd194e0c496ae8b78378cfcf4920ed3eb
COM <COM@DESKTOP-L2F7LSG>
2020-12-02 13:00:25 +0900

    형모오빠가 째려본다

--- src/main/java/com/mycompany/webapp/controller/CommunityController.java
+++ src/main/java/com/mycompany/webapp/controller/CommunityController.java
@@ -75,18 +75,18 @@ public class CommunityController {
 	}
 
 	@GetMapping("/comm_list")
-	public String Comm_list(Model model, int check, String search,@RequestParam(defaultValue="1")int pageNo) {
+	public String Comm_list(Model model, int check, String search, @RequestParam(defaultValue = "1") int pageNo) {
 
 		// 검색으로 컨트롤러를 호출한건지 확인!
 		if (check == 1) {
-			
+
 			String temp = "%";
 			temp += search + "%";
 			int rows = service.Comm_listLow(temp);
-			Pager pager = new Pager(5, 5,rows, pageNo);
+			Pager pager = new Pager(5, 5, rows, pageNo);
 			pager.setTemp(temp);
 			List<Community> comm_list = service.Comm_search(pager);
-			model.addAttribute("pager",pager);
+			model.addAttribute("pager", pager);
 			model.addAttribute("comm_list", comm_list);
 			return "community/communitylist";
 		}
@@ -97,9 +97,9 @@ public class CommunityController {
 			return "community/communitylistHits";
 		}
 		int rows = service.Comm_listLow();
-		Pager pager = new Pager(5, 5,rows, pageNo);
-		List<Community> comm_list = service.Comm_list(pager);// 전체리스트		
-		model.addAttribute("pager",pager);
+		Pager pager = new Pager(5, 5, rows, pageNo);
+		List<Community> comm_list = service.Comm_list(pager);// 전체리스트
+		model.addAttribute("pager", pager);
 		model.addAttribute("comm_list", comm_list);
 		return "community/communitylist";
 
@@ -107,7 +107,7 @@ public class CommunityController {
 
 	@GetMapping("/comm_listphoto")
 	public void download(String fileName, HttpServletRequest request, HttpServletResponse response) throws Exception {
-	
+
 		// 파일의 데이터를 읽기 위한 입력 스트림 얻기
 		String saveFilePath = "C:/Temp/upload/community/" + fileName;
 		InputStream is = new FileInputStream(saveFilePath);
@@ -134,33 +134,33 @@ public class CommunityController {
 		is.close();
 
 	}
-	
-	
+
 	@GetMapping("/comm_detail")
-	public String Comm_Detail(int cnumber, String cmnickname, Model model, HttpSession session) {	
-		service.Comm_hits(cnumber);	//조회수		
-		
+	public String Comm_Detail(int cnumber, String cmnickname, Model model, HttpSession session) {
+		service.Comm_hits(cnumber); // 조회수
+
 		Community comm_list = new Community();
 		comm_list.setC_number(cnumber);
 		comm_list.setC_mnickname(cmnickname);
-		comm_list=service.Comm_one(comm_list);
-		logger.info("이미지출력해보자"+comm_list.getMimage());
+		comm_list = service.Comm_one(comm_list);
+		logger.info("이미지출력해보자" + comm_list.getMimage());
 		model.addAttribute("list", comm_list);
 		return "community/comm_detail";
 	}
-	
+
 	@PostMapping("/comm_replyWrite")
-	public void comm_replyWrite(Community comm_list, String rcontent,int c_number,HttpServletResponse response, HttpSession session, Model model) throws Exception {
+	public void comm_replyWrite(Community comm_list, String rcontent, int c_number, HttpServletResponse response,
+			HttpSession session, Model model) throws Exception {
 		logger.info("실행");
-		
+
 		Member member = (Member) session.getAttribute("member");
 		String mnickname = member.getMnickname();
-		
+
 		comm_list.setCr_rmnickname(mnickname);
 		comm_list.setCr_cnumber(c_number);
 		comm_list.setCr_rcontent(rcontent);
 		service.comm_replyWrite(comm_list);
-		
+
 		response.setContentType("application/json; charset=utf-8");
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("result", "success");
@@ -172,12 +172,23 @@ public class CommunityController {
 	}
 	
 	@GetMapping("/comm_replyList")
-	public String comm_replyList(@RequestParam(defaultValue="1")int pageNo, Model model, int pnumber) {
+	public String comm_replyList(@RequestParam(defaultValue="1")int pageNo, Model model, int cnumber) {
 		logger.info("실행");
 		Community comm_list = new Community();
+		comm_list.setCr_cnumber(cnumber);
+		int count = service.getCount(comm_list);
 		
+		Pager pager = new Pager(5, 5, count, pageNo);
+		comm_list.setEndRowNo(pager.getEndRowNo());
+		comm_list.setStartRowNo(pager.getStartRowNo());
 		
+		List<Community> list = service.comm_ReplyList(comm_list);
+		
+		model.addAttribute("replylist", list);
+		model.addAttribute("pager", pager);
+		model.addAttribute("cnumber", cnumber);
+		model.addAttribute("count", count);
 		return "community/comm_detail";
 	}
-
+	
 }
--- src/main/java/com/mycompany/webapp/service/CommunityService.java
+++ src/main/java/com/mycompany/webapp/service/CommunityService.java
@@ -19,13 +19,26 @@ public class CommunityService {
 	private C_ReplyDao c_replyDao;
 	
 	public void comm_write(Community community) {
-		
-		communityDao.communityInsert(community);
-		
+		communityDao.communityInsert(community);	
 	}
 	
 	public int comm_replyWrite(Community community) {
 		return c_replyDao.comm_replyInsert(community);
+	}
+	
+	public int getCount(Community community) {
+			int count = c_replyDao.communityReplyCount(community);
+			return count;
+		}
+	
+	public Community comm_replyList(int cnumber) {
+			Community community = c_replyDao.communityReplyList(cnumber);
+			return community;
+		}
+	
+	public List<Community> comm_ReplyList(Community community) {
+		List<Community> list = c_replyDao.ReplyPage(community);
+		return list;
 	}
 
 	public List<Community> Comm_list(Pager pager) {
