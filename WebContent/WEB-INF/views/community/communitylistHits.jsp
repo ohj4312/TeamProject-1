e7a62442f0dbff9a8ce5b940a7d98e579454f631
COM <COM@DESKTOP-L2F7LSG>
2020-12-06 00:11:20 +0900

    커뮤니티 수정팔로우 추가 시간포맷팅 수정폼 수정

--- WebContent/WEB-INF/views/community/communitylistHits.jsp
+++ WebContent/WEB-INF/views/community/communitylistHits.jsp
@@ -2,12 +2,13 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <h3 class="sidebar-title" style="padding-bottom:4%;">인기글</h3>   
-<c:forEach var="list" items="${comm_list}">		
+<c:forEach var="list" items="${comm_list}">
+<fmt:formatDate var="c_date" value="${list.c_date}" pattern="yyyy-MM-dd HH:mm" />		
               <div class="sidebar-item recent-posts">
                 <div class="post-item clearfix">
                   <img src="<%=application.getContextPath() %>/file/community?fileName=${list.c_image}" alt="">
                   <h4><a href="<%=application.getContextPath() %>/community/comm_detail?cnumber=${list.c_number}&cmnickname=${list.c_mnickname}">${list.c_title}</a></h4>
-                  <time >${list.c_date}</time>
+                  <time >${c_date}</time>
                 </div>                
               </div> 
 </c:forEach>
--- src/main/java/com/mycompany/webapp/controller/CommunityController.java
+++ src/main/java/com/mycompany/webapp/controller/CommunityController.java
@@ -1,17 +1,11 @@
 package com.mycompany.webapp.controller;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -20,17 +14,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.mycompany.webapp.dao.C_ReplyDao;
 import com.mycompany.webapp.dto.Community;
+import com.mycompany.webapp.dto.Follows;
 import com.mycompany.webapp.dto.Member;
 import com.mycompany.webapp.dto.Pager;
 import com.mycompany.webapp.service.CommunityService;
+import com.mycompany.webapp.service.FollowService;
 
 @Controller
 @RequestMapping("/community")
@@ -38,6 +32,8 @@ public class CommunityController {
 	private final Logger logger = LoggerFactory.getLogger(CommunityController.class);
 	@Resource
 	private CommunityService service;
+	@Resource
+	private FollowService followService; 
 
 	@GetMapping("")
 	public String board() {
@@ -123,12 +119,19 @@ public class CommunityController {
 	@GetMapping("/comm_detail")
 	public String Comm_Detail(int cnumber, String cmnickname,Model model, HttpSession session) {	
 		service.Comm_hits(cnumber);	//조회수		
-		
+		Member member=(Member)session.getAttribute("member");
+	
 		Community comm_list = new Community();
 		comm_list.setC_number(cnumber);
 		comm_list.setC_mnickname(cmnickname);
 		comm_list=service.Comm_one(comm_list);
-		
+		if(member!=null) {
+		Follows follow=new Follows();
+		follow.setFollowing(comm_list.getMemail());
+		follow.setFollower(member.getMemail());
+		int result=followService.followCheck(follow);
+		model.addAttribute("follwsCheck",result);
+		}		
 		model.addAttribute("list", comm_list);
 		return "community/comm_detail";
 	}
